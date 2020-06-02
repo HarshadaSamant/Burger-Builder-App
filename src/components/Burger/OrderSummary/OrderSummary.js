8f3096545f1acd9ac161f8ad6473f686fda4b1b7
Harshada Samant <harshada.samant@neosofttech.com>
2020-06-02 19:20:02 +0530

    Added ingredients counter

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Aux from '../../../hoc/Aux';
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igkey => {
+            return (
+                <li>
+                    <span style={{textTransform: 'capitalize'}}>{igkey}</span>: {props.ingredients[igkey]}
+                </li>
+            )
+        })
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicious burder with the following ingredients: </p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+        </Aux>
+    );
+};
+
+export default orderSummary;
